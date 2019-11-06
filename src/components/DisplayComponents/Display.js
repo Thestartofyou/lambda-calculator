eab7daf0b5584b2f108b15bd8bf902c43a6593e0
leifgerchberg <leifgerchberg@gmail.com>
2019-11-06 09:46:35 -0500

    information

--- src/components/DisplayComponents/Display.js
+++ src/components/DisplayComponents/Display.js
@@ -1,5 +1,11 @@
 import React from "react";
 
-const Display = () => {
-  return <div className="display">{/* Display any props data here */}</div>;
+const Display = (props) => {
+  return (
+    <div className="display">
+      <div className="display-value">{props.displayValue}</div>
+    </div>
+  );
 };
+
+export default Display;
